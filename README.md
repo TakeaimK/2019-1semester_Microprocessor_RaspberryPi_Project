e7445d0826976c796546d381885467954c942a5c
TakeaimK <44010902+TakeaimK@users.noreply.github.com>
2019-11-05 12:19:24 +0900

    2019-1sem M.P. RaspberryPi

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # 2019-1semester_Microprocessor_RaspberryPi_Project
-2019-1학기 마이크로프로세서및실험 라즈베리파이 활용 프로젝트
+
+### 2019-1학기 마이크로프로세서및실험 라즈베리파이 활용 프로젝트 - Kiosk 제작
+
+![KakaoTalk_20190530_103742420](https://user-images.githubusercontent.com/44010902/68175886-10aea800-ffc6-11e9-87c0-46a80fed61b9.png)
+![KakaoTalk_20190530_103742420_06](https://user-images.githubusercontent.com/44010902/68175887-10aea800-ffc6-11e9-9fe9-a7b2c48e3c8c.png)
+![KakaoTalk_20190530_103742420_08](https://user-images.githubusercontent.com/44010902/68175888-10aea800-ffc6-11e9-9a4b-4309939806d9.png)
+
+---
+
+IDE : Eclips Oxygen  
+Language : Java(Client), Python(Server)
+Adviser : Kim Young-cheon
+
+### Team Member
+
+Server : Park Kyung-hyun, Seo Eun-bin  
+Client : Choi Se-jin(Leader), Gong Ji-na
+
+### How to use
+
+1. Install JVM on Raspberry Pi
+2. Run Server and Client
+
+---
+
+라즈베리파이 활용 프로젝트입니다.  
+저희 팀은 Kiosk (무인 주문기)를 제작하였으며, 회전초밥 가게를 컨셉으로 잡고 제작하였습니다.  
+먼저 Server는 소켓 통신을 활용하여 일반 PC에서 실행 가능한 Python Program으로 제작되었습니다. 이곳에서는 Client에서 주문하기를 누르면, Server의 Console에 Client로부터 전송된 문자열 데이터를 분리하여 주문 주문한 테이블과 품목, 수량이 출력됩니다.  
+Client는 ARM Processor에서도 구동 가능하도록 Java Swing으로 제작된 GUI Client이며, 실행 시 메뉴 주문 화면이 나오고, 주문 완료 시 Server로 문자열 형태의 데이터가 전송됩니다.  
+장점으로는 구축 비용이 다른 Kiosk보다 저렴합니다. Server부터 Client까지 하드웨어 비용이 매우 저렴합니다.
---
+++ foodGUI.java
@@ -0,0 +1,263 @@
+package raspberryPi_GUI;
+ 
+import java.awt.BorderLayout;
+import java.awt.Button;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Label;
+import java.awt.Panel;
+import java.awt.TextArea;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+ 
+public class foodGUI {
+    int count = 0;
+    String show = "";
+ 
+    public foodGUI() {
+ 
+        // 디자인단
+        // 프레임 설정단
+        JFrame frame = new JFrame("초밥 자동 판매기");
+        frame.setBounds(0, 0, 764, 450);
+        frame.setBackground(Color.white);
+ 
+        // 폰트
+        Font font = new Font("나눔고딕", Font.PLAIN, 15);
+ 
+        // 북쪽
+        Panel pNorth = new Panel();
+        pNorth.setBackground(new Color(0, 0, 0));
+        pNorth.setLayout(null);
+        pNorth.setSize(0, 500);
+        pNorth.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 15));
+ 
+        // 배열 설정 부분
+        String menu[] = { "salmon", "flat", "tuna", "eel", "beef", "cuttle"};
+        int price[] = { 3000, 2500, 3500, 3000, 2800, 2500};
+        JButton bt[] = new JButton[menu.length];
+        TextField suja[] = new TextField[menu.length];
+        Button minus[] = new Button[menu.length];
+        Button plus[] = new Button[menu.length];
+        JButton ok[] = new JButton[menu.length];
+        Label l[] = new Label[menu.length];
+        ImageIcon icon[] = new ImageIcon[menu.length];
+ 
+        // 버튼 설정 부분
+        for (int i = 0; i < menu.length; i++) {
+ 
+            // 초밥 버튼
+            bt[i] = new JButton(menu[i]);
+            if (i < 3) {
+                bt[i].setBounds(25 + i * 150, 50, 100, 80);
+            } else {
+                bt[i].setBounds(25 + (i -3) * 150, 230, 100, 80);
+            }
+            bt[i].setVerticalAlignment(SwingConstants.BOTTOM);
+            icon[i] = new ImageIcon("images/icon/" + menu[i] + ".jpg");
+            bt[i].setIcon(icon[i]);
+ 
+            // 숫자 txt 버튼
+            suja[i] = new TextField("0");
+            suja[i].setBackground(Color.white);
+            suja[i].setEditable(false);
+            suja[i].setBounds(bt[i].getX() + 30, bt[i].getY() + 100, 40, 20);
+ 
+            // "-" 버튼
+            minus[i] = new Button("-");
+            minus[i].setBounds(bt[i].getX(), suja[i].getY(), 20, 20);
+            minus[i].setEnabled(false);
+ 
+            // "+" 버튼
+            plus[i] = new Button("+");
+            plus[i].setBounds(bt[i].getX() + (100 - 20), suja[i].getY(), 20, 20);
+            plus[i].setEnabled(false);
+ 
+            // 가격
+            l[i] = new Label(menu[i] +" "+ price[i] +"");
+            l[i].setFont(font);
+            l[i].setForeground(new Color(255, 255, 255));
+            l[i].setBounds(bt[i].getX() -1, suja[i].getY() - 18, 100, 20);
+ 
+            // 확인 버튼
+            ok[i] = new JButton("확인");
+            ok[i].setBounds(bt[i].getX(), suja[i].getY() + 20, 100, 20);
+            ok[i].setEnabled(false);
+ 
+            pNorth.add(bt[i]);
+            pNorth.add(suja[i]);
+            pNorth.add(minus[i]);
+            pNorth.add(plus[i]);
+            pNorth.add(l[i]);
+            pNorth.add(ok[i]);
+        }
+ 
+        // 남쪽
+        Panel pSouth = new Panel();
+        pSouth.setForeground(new Color(255, 255, 255));
+        pSouth.setFont(font);
+        pSouth.setBackground(new Color(255, 255, 255));
+ 
+ 
+        // 컴포넌트
+        frame.getContentPane().add(pNorth, BorderLayout.NORTH);
+        
+               // 중앙
+               TextArea ta = new TextArea("", 0, 0, TextArea.SCROLLBARS_VERTICAL_ONLY);
+               ta.setBounds(502, 28, 225, 250);
+               pNorth.add(ta);
+               ta.setText("상품명    합계   수량\r\n\r\n");
+               ta.setBackground(Color.white);
+               ta.setEditable(false);
+               ta.setFont(new Font("Monospaced", Font.BOLD, 15));
+               
+                      Button bt1 = new Button("Order");
+                      bt1.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 12));
+                      bt1.setBounds(502, 351, 51, 30);
+                      pNorth.add(bt1);
+                      Button bt2 = new Button("Reset");
+                      bt2.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 12));
+                      bt2.setBounds(575, 351, 69, 30);
+                      pNorth.add(bt2);
+                      Button bt3 = new Button("Exit");
+                      bt3.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 12));
+                      bt3.setBounds(675, 351, 51, 30);
+                      pNorth.add(bt3);
+                      
+                      JLabel lblWelcomeToSusi = new JLabel("Welcome to Sushi Nara!");
+                      lblWelcomeToSusi.setForeground(new Color(255, 255, 255));
+                      lblWelcomeToSusi.setHorizontalAlignment(SwingConstants.CENTER);
+                      lblWelcomeToSusi.setFont(new Font("나눔손글씨 펜", Font.PLAIN, 24));
+                      lblWelcomeToSusi.setBounds(66, 10, 345, 30);
+                      pNorth.add(lblWelcomeToSusi);
+                      
+                      
+                             //bt3 닫기버튼
+                             
+                             bt3.addActionListener(new ActionListener() {
+                                 
+                                 @Override
+                                 public void actionPerformed(ActionEvent e) {
+                                     System.exit(0);
+                                 }
+                             });
+                      
+                             // 초기화 버튼
+                             bt2.addActionListener(new ActionListener() {
+                      
+                                 @Override
+                                 public void actionPerformed(ActionEvent e) {
+                                     for (int i = 0; i < menu.length; i++) {
+                                         bt[i].setEnabled(true);
+                                         minus[i].setEnabled(false);
+                                         plus[i].setEnabled(false);
+                                         suja[i].setText("0");
+                                         ta.setText("상품명    합계   수량\n\n");
+                      
+                                     }
+                                 }
+                             });
+                      
+                             // 주문버튼
+                             bt1.addActionListener(new ActionListener() {
+                      
+                                 @Override
+                                 public void actionPerformed(ActionEvent e) {
+                                     JOptionPane.showMessageDialog(null, ta.getText() + " 주문되었습니다. \n이용해주셔서 감사합니다.");
+                                     for (int i = 0; i < menu.length; i++) {
+                                         bt[i].setEnabled(true);
+                                         minus[i].setEnabled(false);
+                                         plus[i].setEnabled(false);
+                                         suja[i].setText("0");
+                                         ta.setText("상품명    합계    수량\n\n");
+                      
+                                     }
+                                 }
+                             });
+        frame.getContentPane().add(pSouth, BorderLayout.SOUTH);
+        frame.setVisible(true);
+ 
+        // 이벤트단
+        for (int i = 0; i < menu.length; i++) {
+            int j = i;
+ 
+            // 초밥 버튼 이벤트
+            bt[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    minus[j].setEnabled(true);
+                    plus[j].setEnabled(true);
+                    bt[j].setEnabled(false);
+                    ok[j].setEnabled(true);
+ 
+                    count = 0;
+                }
+            });
+ 
+            // "-" 버튼 이벤트
+            minus[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if (count > 0) {
+                        count = count - 1;
+                        suja[j].setText(count + "");
+                        ok[j].setEnabled(true);
+                    } else {
+                        minus[j].setEnabled(false);
+                    }
+                }
+            });
+            
+            // "+" 버튼 이벤트
+            plus[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    count = count + 1;
+                    suja[j].setText(count + "");
+                    ok[j].setEnabled(true);
+                    if (count > 0) {
+                        minus[j].setEnabled(true);
+                    }
+                }
+            });
+            
+            //확인 버튼 이벤트
+            ok[i].addActionListener(new ActionListener() {
+ 
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    show = bt[j].getActionCommand();
+                    ta.append(" " + show + "  " + price[j] + "  " + count + "\n");
+                    ok[j].setEnabled(false);
+                }
+            });
+ 
+        }
+ 
+        // 끄기
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+ 
+    // 메인
+    public static void main(String[] args) {
+        new foodGUI();
+    }
+}
+
+
